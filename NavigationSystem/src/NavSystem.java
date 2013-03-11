6a8d4cad174be3299339861e847e5207bd67d9f4
Brian Anderson <banderson623@gmail.com>
2013-03-11 01:09:44 -0500

    initial
    !

---
+++ NavigationSystem/src/NavSystem.java
@@ -0,0 +1,245 @@
+import javax.swing.*;
+import java.util.Random;
+
+public class NavSystem
+{
+  public static void main(String[] args)
+  {
+    final ManeuverGen mg = new ManeuverGen();
+    FakeDataGenerator gen = new FakeDataGenerator();
+    final RouteCalculator rc = new RouteCalculator(mg, gen);
+    
+    // create and start UI...
+    Runnable r = new Runnable()
+    {
+      public void run()
+      {
+        UI ui = UI.createAndShow(rc);
+        mg.attachUI(ui);
+      }
+    };
+    SwingUtilities.invokeLater(r);
+
+    new GPS(mg, gen).start();
+  }
+}
+
+class ManeuverGen
+{
+  private Route currentRoute;
+  private Position currentPosition;
+  private UI ui;
+  private Random rand = new Random();
+  public void attachUI(UI ui)
+  {
+    this.ui = ui;
+  }
+
+  public void setNewRoute(Route r)
+  {
+    currentRoute = r;
+    ui.setNewRoute(r);
+  }
+
+  public void updatePosition(Position p)
+  {
+    currentPosition = p;
+    ui.setPosition(p);
+    Instruction inst = nextManeuver();
+    if (inst != null)
+    {
+      ui.announceNextTurn(inst);
+    }
+  }
+
+  public Position getCurrentPosition()
+  {
+    return currentPosition;
+  }
+
+  private Instruction nextManeuver()
+  {
+    if (currentRoute == null)
+    {
+      return new Instruction("No route selected");
+    }
+    else if (currentRoute.getDest().equals(currentPosition))
+    {
+      return new Instruction("Arrived!");
+    }
+    else
+    {
+      String dir = rand.nextInt(2) == 0 ? "left" : "right";
+      int dist = 100 * (rand.nextInt(4) + 1);
+      return new Instruction("Turn " + dir + " in " + dist + " meters");
+    }
+  }
+}
+
+class RouteCalculator
+{
+  private ManeuverGen mg;
+  private FakeDataGenerator gen;
+  
+  public RouteCalculator(ManeuverGen mg, FakeDataGenerator gen)
+  {
+    this.mg = mg;
+    this.gen = gen;
+  }
+
+  public void calculateRoute(Position dst)
+  {
+    Route r = doCalculate(mg.getCurrentPosition(), dst);
+    mg.setNewRoute(r);
+    gen.setDest(dst.getCoordinate());
+  }
+
+  // This operation may take a long time (e.g. minutes)
+  private Route doCalculate(Position src, Position dst)
+  {
+    lookBusy(10000);
+    return new Route(src, dst);
+  }
+
+  private static void lookBusy(long millis)
+  {
+    long interval = 300;
+    long stop = System.currentTimeMillis() + millis;
+
+    try
+    {
+      while (!Thread.currentThread().isInterrupted()
+          && System.currentTimeMillis() < stop)
+      {
+        System.out.print(".");
+        Thread.sleep(interval);
+      }
+    }
+    catch (InterruptedException ie)
+    {
+    }
+  }
+}
+
+class GPS extends Thread
+{
+  private ManeuverGen mg;
+  private FakeDataGenerator gen;
+
+  public GPS(ManeuverGen mg, FakeDataGenerator gen)
+  {
+    this.mg = mg;
+    this.gen = gen;
+  }
+
+  public void run()
+  {
+    while (true)
+    {
+      Position p = gen.readData();
+      mg.updatePosition(p);
+    }
+  }
+
+}
+
+class Position
+{
+  private final int p;
+
+  public Position(int p)
+  {
+    this.p = p;
+  }
+
+  public int getCoordinate()
+  {
+    return p;
+  }
+  
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (obj == null || obj.getClass() != Position.class) return false;
+    return (p == ((Position) obj).p);
+  }
+  
+  @Override
+  public String toString()
+  {
+    return "" + p;
+  }
+}
+
+class Route
+{
+  private final Position src;
+  private final Position dst;
+
+  public Route(Position src, Position dst)
+  {
+    this.src = src;
+    this.dst = dst;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "From " + src + " to " + dst;
+  }
+  
+  public Position getSource()
+  {
+    return src;
+  }
+  
+  public Position getDest()
+  {
+    return dst;
+  }
+}
+
+class Instruction
+{
+  private final String inst;
+
+  public Instruction(String inst)
+  {
+    this.inst = inst;
+  }
+
+  String decode()
+  {
+    return inst;
+  }
+}
+
+// since we don't have a real satellite or real car
+class FakeDataGenerator
+{
+  private int pos;
+  private int dest;
+  
+  public synchronized void setDest(int dest)
+  {
+    this.dest = dest;
+  }
+  
+  public Position readData()
+  {
+    try
+    {
+      Thread.sleep(3000);
+    }
+    catch (InterruptedException ie)
+    {
+    }
+    synchronized (this)
+    {
+      // pretend to get closer to "destination"
+      if (pos < dest) ++pos;
+      else if (pos > dest) --pos;
+      return new Position(pos);
+    }
+  }
+}
---
+++ NavigationSystem/src/UI.java
@@ -0,0 +1,116 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UI extends JPanel { 
+    private RouteCalculator rc;
+    private Position currentPosition = new Position(0);
+    private Position currentDestination = new Position(0);
+    private Route currentRoute;
+    
+    private JPanel mapDisplay;
+    private JButton confirmDestinationButton;
+    private JLabel nextTurn;
+    private JTextField routeText;
+    
+    public static UI createAndShow(RouteCalculator rc)
+    {
+      // create the frame
+      JFrame frame = new JFrame("Nav system");
+      UI ui = new UI(rc);
+        // create an instance of our JPanel subclass and 
+        // add it to the frame
+        frame.getContentPane().add(ui);
+
+        // use the preferred sizes
+        frame.pack();
+
+        // we want to shut down the application if the 
+        // "close" button is pressed on the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // make the frame visible and start the UI machinery
+        frame.setVisible(true);
+        
+        return ui;
+
+    }
+    
+    public UI(RouteCalculator rc)
+    {
+        this.rc = rc;
+        mapDisplay = new MapDisplay();
+        mapDisplay.setPreferredSize(new Dimension(500, 200));
+        confirmDestinationButton = new JButton("Set Destination");
+        nextTurn = new JLabel();
+        routeText = new JTextField(10);
+        confirmDestinationButton.addActionListener(new DestinationButtonListener());
+        
+        // lay out the pieces vertically
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.add(mapDisplay);
+        this.add(nextTurn);
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.add(routeText);
+        bottomPanel.add(confirmDestinationButton);
+        this.add(bottomPanel);
+    }
+    
+    public void setPosition(Position p) {
+        currentPosition = p;
+        repaint();
+    }
+
+    public void setNewRoute(Route r)
+    {
+        currentRoute = r;
+        confirmDestinationButton.setEnabled(true);
+        repaint();
+    }
+    public void announceNextTurn(Instruction inst) {
+        nextTurn.setText(inst.decode());
+        repaint();
+    }
+
+    // listener attached to confirmDestinationButton...
+    private class DestinationButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+          nextTurn.setText("Calculating new route...");
+          String routeString = routeText.getText();
+          try
+          {
+            int dest = Integer.parseInt(routeString);
+            currentDestination = new Position(dest);
+            confirmDestinationButton.setEnabled(false);
+            rc.calculateRoute(currentDestination);
+          }
+          catch (NumberFormatException nfe)
+          {
+            // do nothing
+          }
+        }
+    }
+    
+    /**
+     * Pretends to draw a map showing current position and route...
+     */
+    private class MapDisplay extends JPanel
+    {
+        public void paintComponent(Graphics g)
+        {
+          Graphics2D g2 = (Graphics2D) g;
+          g2.setBackground(Color.WHITE);
+          g2.clearRect(0, 0, getWidth(), getHeight());
+          g2.drawRect(0, 0, getWidth(), getHeight());
+          g2.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 24));
+          String pos = "Current position: " + currentPosition.getCoordinate();
+          String dest = "Current destination: " + currentDestination.getCoordinate();
+          String route = "Route: " + (currentRoute == null ? "No route selected" : currentRoute.toString());
+          g2.drawString(pos, 30, 30);
+          g2.drawString(dest, 30, 60);
+          g2.drawString(route, 30, 90);
+            
+        }
+    }
+}
