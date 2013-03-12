c1f14930bb17f32a6d8d8d2518b2cabbbf3a1aac
Brian Anderson <banderson623@gmail.com>
2013-03-11 22:36:37 -0500

    revised to meet the criteria of the assignment

--- NavigationSystem/src/NavSystem.java
+++ NavigationSystem/src/NavSystem.java
@@ -3,242 +3,265 @@ import java.util.Random;
 
 public class NavSystem
 {
-  public static void main(String[] args)
-  {
-    final ManeuverGen manueverGenerator = new ManeuverGen();
-    FakeDataGenerator dataGenerator = new FakeDataGenerator();
-    final RouteCalculator rc = new RouteCalculator(manueverGenerator, dataGenerator);
-    
-    // create and start UI...
-    Runnable r = new Runnable()
+    public static void main(String[] args)
     {
-      public void run()
-      {
-        UI ui = UI.createAndShow(rc);
-        manueverGenerator.attachUI(ui);
-      }
-    };
-    SwingUtilities.invokeLater(r);
-    new GPS(manueverGenerator, dataGenerator).start();
-  }
+        final ManeuverGen manueverGenerator = new ManeuverGen();
+        FakeDataGenerator dataGenerator = new FakeDataGenerator();
+        final RouteCalculator rc = new RouteCalculator(manueverGenerator, dataGenerator);
+
+        // create and start UI...
+        Runnable r = new Runnable()
+        {
+            public void run()
+            {
+                UI ui = UI.createAndShow(rc);
+                manueverGenerator.attachUI(ui);
+            }
+        };
+        SwingUtilities.invokeLater(r);
+        new GPS(manueverGenerator, dataGenerator).start();
+    }
 }
 
+
+/**
+ * Given a route, it produces a set of maneuvers
+ * Attaches to the UI, yuck.
+ */
 class ManeuverGen
 {
-  private Route currentRoute;
-  private Position currentPosition;
-  private UI ui;
-  private Random rand = new Random();
-  public void attachUI(UI ui)
-  {
-    this.ui = ui;
-  }
+    private Route currentRoute;
+    // Changed: the null string was bugging me.
+    private Position currentPosition = new Position(0);
+    private UI ui;
+    private Random rand = new Random();
 
-  public void setNewRoute(Route r)
-  {
-    currentRoute = r;
-    ui.setNewRoute(r);
-  }
+    // Changed: added synchronized because this object
+    // may be accessed by multiple threads
+    public synchronized void attachUI(UI ui)
+    {
+        this.ui = ui;
+    }
 
-  public void updatePosition(Position p)
-  {
-    currentPosition = p;
-    ui.setPosition(p);
-    Instruction inst = nextManeuver();
-    if (inst != null)
+    // Changed: added synchronized because this object
+    // may be accessed by multiple threads
+    public synchronized void  setNewRoute(Route r)
     {
-      ui.announceNextTurn(inst);
+        currentRoute = r;
+        ui.setNewRoute(r);
     }
-  }
 
-  public Position getCurrentPosition()
-  {
-    return currentPosition;
-  }
+    // Changed: added synchronized because this object
+    // may be accessed by multiple threads
+    public synchronized void updatePosition(Position p)
+    {
+        currentPosition = p;
+        ui.setPosition(p);
+        Instruction inst = nextManeuver();
+        if (inst != null)
+        {
+            ui.announceNextTurn(inst);
+        }
+    }
+    // Changed: added synchronized because this object
+    // may be accessed by multiple threads
+    public synchronized Position getCurrentPosition()
+    {
+        return currentPosition;
+    }
 
-  private Instruction nextManeuver()
-  {
-    if (currentRoute == null)
+    // Changed: added synchronized because this object
+    // may be accessed by multiple threads
+    private Instruction nextManeuver()
     {
-      return new Instruction("No route selected");
+        String nextInstructionString;
+        if(currentRoute == null)
+        {
+            nextInstructionString = "No route selected";
+        }
+        else if(currentRoute.getDest().equals(currentPosition))
+        {
+            nextInstructionString = "Arrived!";
+        }
+        else
+        {
+            String dir = rand.nextInt(2) == 0 ? "left" : "right";
+            int dist = 100 * (rand.nextInt(4) + 1);
+            nextInstructionString = "Turn " + dir + " in " + dist + " meters";
+        }
+        // I really dislike multiple return statements in a function
+        // this fixes that.
+        return new Instruction(nextInstructionString);
     }
-    else if (currentRoute.getDest().equals(currentPosition))
-    {
-      return new Instruction("Arrived!");
-    }
-    else
-    {
-      String dir = rand.nextInt(2) == 0 ? "left" : "right";
-      int dist = 100 * (rand.nextInt(4) + 1);
-      return new Instruction("Turn " + dir + " in " + dist + " meters");
-    }
-  }
 }
+
 
 class RouteCalculator
 {
-  private ManeuverGen mg;
-  private FakeDataGenerator gen;
-  
-  public RouteCalculator(ManeuverGen mg, FakeDataGenerator gen)
-  {
-    this.mg = mg;
-    this.gen = gen;
-  }
+    private ManeuverGen mg;
+    private FakeDataGenerator gen;
 
-  public void calculateRoute(Position dst)
-  {
-    Route r = doCalculate(mg.getCurrentPosition(), dst);
-    mg.setNewRoute(r);
-    gen.setDest(dst.getCoordinate());
-  }
-
-  // This operation may take a long time (e.g. minutes)
-  private Route doCalculate(Position src, Position dst)
-  {
-    lookBusy(10000);
-    return new Route(src, dst);
-  }
-
-  private static void lookBusy(long millis)
-  {
-    long interval = 300;
-    long stop = System.currentTimeMillis() + millis;
-
-    try
+    public RouteCalculator(ManeuverGen mg, FakeDataGenerator gen)
     {
-      while (!Thread.currentThread().isInterrupted()
-          && System.currentTimeMillis() < stop)
-      {
-        System.out.print(".");
-        Thread.sleep(interval);
-      }
+        this.mg = mg;
+        this.gen = gen;
     }
-    catch (InterruptedException ie)
+
+    public void calculateRoute(Position dst)
     {
+        Route r = doCalculate(mg.getCurrentPosition(), dst);
+        mg.setNewRoute(r);
+        gen.setDest(dst.getCoordinate());
     }
-  }
+
+    // This operation may take a long time (e.g. minutes)
+    private Route doCalculate(Position src, Position dst)
+    {
+        lookBusy(10000);
+        return new Route(src, dst);
+    }
+
+    private static void lookBusy(long millis)
+    {
+        long interval = 300;
+        long stop = System.currentTimeMillis() + millis;
+
+        try
+        {
+            while (!Thread.currentThread().isInterrupted()
+                    && System.currentTimeMillis() < stop)
+            {
+                System.out.print(".");
+                Thread.sleep(interval);
+            }
+        } catch (InterruptedException ignored)
+        {
+        }
+    }
 }
 
 class GPS extends Thread
 {
-  private ManeuverGen mg;
-  private FakeDataGenerator gen;
+    private ManeuverGen mg;
+    private FakeDataGenerator gen;
 
-  public GPS(ManeuverGen mg, FakeDataGenerator gen)
-  {
-    this.mg = mg;
-    this.gen = gen;
-  }
-
-  public void run()
-  {
-    while (true)
+    public GPS(ManeuverGen mg, FakeDataGenerator gen)
     {
-      Position p = gen.readData();
-      mg.updatePosition(p);
+        this.mg = mg;
+        this.gen = gen;
     }
-  }
+
+    public void run()
+    {
+        while (true)
+        {
+            Position p = gen.readData();
+            mg.updatePosition(p);
+        }
+    }
 
 }
 
 class Position
 {
-  private final int p;
+    private final int p;
 
-  public Position(int p)
-  {
-    this.p = p;
-  }
+    public Position(int p)
+    {
+        this.p = p;
+    }
 
-  public int getCoordinate()
-  {
-    return p;
-  }
-  
-  @Override
-  public boolean equals(Object obj)
-  {
-    if (obj == null || obj.getClass() != Position.class) return false;
-    return (p == ((Position) obj).p);
-  }
-  
-  @Override
-  public String toString()
-  {
-    return "" + p;
-  }
+    public int getCoordinate()
+    {
+        return p;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null || obj.getClass() != Position.class)
+            return false;
+        return (p == ((Position) obj).p);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "" + p;
+    }
 }
 
 class Route
 {
-  private final Position src;
-  private final Position dst;
+    private final Position src;
+    private final Position dst;
 
-  public Route(Position src, Position dst)
-  {
-    this.src = src;
-    this.dst = dst;
-  }
+    public Route(Position src, Position dst)
+    {
+        this.src = src;
+        this.dst = dst;
+    }
 
-  @Override
-  public String toString()
-  {
-    return "From " + src + " to " + dst;
-  }
-  
-  public Position getSource()
-  {
-    return src;
-  }
-  
-  public Position getDest()
-  {
-    return dst;
-  }
+    @Override
+    public String toString()
+    {
+        // Added an extra 0 here.
+        return "From " + src + " to " + dst;
+    }
+
+    public Position getSource()
+    {
+        return src;
+    }
+
+    public Position getDest()
+    {
+        return dst;
+    }
 }
 
 class Instruction
 {
-  private final String inst;
+    private final String inst;
 
-  public Instruction(String inst)
-  {
-    this.inst = inst;
-  }
+    public Instruction(String inst)
+    {
+        this.inst = inst;
+    }
 
-  String decode()
-  {
-    return inst;
-  }
+    String decode()
+    {
+        return inst;
+    }
 }
 
 // since we don't have a real satellite or real car
 class FakeDataGenerator
 {
-  private int pos;
-  private int dest;
-  
-  public synchronized void setDest(int dest)
-  {
-    this.dest = dest;
-  }
-  
-  public Position readData()
-  {
-    try
+    private int pos;
+    private int dest;
+
+    public synchronized void setDest(int dest)
     {
-      Thread.sleep(3000);
+        this.dest = dest;
     }
-    catch (InterruptedException ie)
+
+    public Position readData()
     {
+        try
+        {
+            Thread.sleep(3000);
+        } catch (InterruptedException ignored)
+        {
+        }
+        synchronized (this)
+        {
+            // pretend to get closer to "destination"
+            if (pos < dest)
+                ++pos;
+            else if (pos > dest)
+                --pos;
+            return new Position(pos);
+        }
     }
-    synchronized (this)
-    {
-      // pretend to get closer to "destination"
-      if (pos < dest) ++pos;
-      else if (pos > dest) --pos;
-      return new Position(pos);
-    }
-  }
 }
--- NavigationSystem/src/UI.java
+++ NavigationSystem/src/UI.java
@@ -3,22 +3,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UI extends JPanel { 
+public class UI extends JPanel
+{
     private RouteCalculator rc;
     private Position currentPosition = new Position(0);
     private Position currentDestination = new Position(0);
     private Route currentRoute;
-    
+
     private JPanel mapDisplay;
     private JButton confirmDestinationButton;
     private JLabel nextTurn;
     private JTextField routeText;
-    
+
+    private boolean isProcessingNewRoute = false;
+
+    private GetRouteCalculationWorker routerUIWorker;
+
     public static UI createAndShow(RouteCalculator rc)
     {
-      // create the frame
-      JFrame frame = new JFrame("Nav system");
-      UI ui = new UI(rc);
+        // create the frame
+        JFrame frame = new JFrame("Nav system");
+        UI ui = new UI(rc);
         // create an instance of our JPanel subclass and 
         // add it to the frame
         frame.getContentPane().add(ui);
@@ -32,11 +37,11 @@ public class UI extends JPanel {
 
         // make the frame visible and start the UI machinery
         frame.setVisible(true);
-        
+
         return ui;
 
     }
-    
+
     public UI(RouteCalculator rc)
     {
         this.rc = rc;
@@ -46,7 +51,7 @@ public class UI extends JPanel {
         nextTurn = new JLabel();
         routeText = new JTextField(10);
         confirmDestinationButton.addActionListener(new DestinationButtonListener());
-        
+
         // lay out the pieces vertically
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.add(mapDisplay);
@@ -56,49 +61,88 @@ public class UI extends JPanel {
         bottomPanel.add(confirmDestinationButton);
         this.add(bottomPanel);
     }
-    
-    public void setPosition(Position p) {
-        currentPosition = p;
-        repaint();
+
+    public void setPosition(final Position p)
+    {
+        // Changed to use an anonymous runnable class inside
+        // SwingUtilities.invokeLater so that it is processed
+        // properly by the swing thread.
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                currentPosition = p;
+                repaint();
+            }
+        });
     }
 
-    public void setNewRoute(Route r)
+    public void setNewRoute(final Route r)
     {
-        System.out.println("Calling new route with r: " + r);
-        currentRoute = r;
-        confirmDestinationButton.setEnabled(true);
-        repaint();
+        // Created a new Runnable to be invoked
+        // within the swing-friendly GUI thread.
+        // because this is called by a Maneuver object
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                currentRoute = r;
+                confirmDestinationButton.setEnabled(true);
+                repaint();
+            }
+        });
     }
-    public void announceNextTurn(Instruction inst) {
-        nextTurn.setText(inst.decode());
-        repaint();
+
+    public void announceNextTurn(final Instruction inst)
+    {
+        // Changed to use an anonymous runnable class inside
+        // SwingUtilities.invokeLater so that it is processed
+        // properly by the swing thread.
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                String text = inst.decode();
+                if(isProcessingNewRoute)
+                {
+                    // Added a small section to note when
+                    // a (re)calculation is happening.
+                    text += " (calculating new route)";
+                }
+                nextTurn.setText(text);
+                repaint();
+            }
+        });
     }
 
     // listener attached to confirmDestinationButton...
-    private class DestinationButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            // Update the UI immediately...
-        nextTurn.setText("Calculating new route...");
-            
-        String routeString = routeText.getText();
-        try
+    private class DestinationButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
         {
-            GetRouteCalculationWorker rcWorker = new GetRouteCalculationWorker(routeString);
-            rcWorker.execute();
-
-//            int dest = Integer.parseInt(routeString);
-//            currentDestination = new Position(dest);
-//            confirmDestinationButton.setEnabled(false);
-//            rc.calculateRoute(currentDestination);
-
-        }
-        catch (NumberFormatException nfe)
-        {
-        // do nothing
-        }
+            // Update the UI immediately... with the following text
+            String routeString = routeText.getText();
+            try
+            {
+                if(routerUIWorker != null && !(routerUIWorker.isDone() ||
+                                               routerUIWorker.isCancelled()))
+                {
+                    // If we are still running lets cancel this and accept
+                    // a new route calculation request
+                    routerUIWorker.cancel(true);
+                }
+                // dispatches to a friendly swing thread.
+                routerUIWorker = new GetRouteCalculationWorker(routeString);
+                routerUIWorker.execute();
+//                rcWorker.cancel(false);
+            } catch (NumberFormatException nfe)
+            {
+                nextTurn.setText("Error: " + routeString + " is not recognized as an integer");
+                // do a little more than nothing
+            }
         }
     }
-    
+
     /**
      * Pretends to draw a map showing current position and route...
      */
@@ -106,29 +150,29 @@ public class UI extends JPanel {
     {
         public void paintComponent(Graphics g)
         {
-            System.out.println("Calling paintComponent");
-          Graphics2D g2 = (Graphics2D) g;
-          g2.setBackground(Color.WHITE);
-          g2.clearRect(0, 0, getWidth(), getHeight());
-          g2.drawRect(0, 0, getWidth(), getHeight());
-          g2.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 24));
-          String pos = "Current position: " + currentPosition.getCoordinate();
-          String dest = "Current destination: " + currentDestination.getCoordinate();
-          String route = "Route: " + (currentRoute == null ? "No route selected" : currentRoute.toString());
-          g2.drawString(pos, 30, 30);
-          g2.drawString(dest, 30, 60);
-          g2.drawString(route, 30, 90);
-            
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setBackground(Color.WHITE);
+            g2.clearRect(0, 0, getWidth(), getHeight());
+            g2.drawRect(0, 0, getWidth(), getHeight());
+            g2.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 24));
+            String pos = "Current position: " + currentPosition.getCoordinate();
+            String dest = "Current destination: " + currentDestination.getCoordinate();
+            String route = "Route: " + (currentRoute == null ? "No route selected" : currentRoute.toString());
+            g2.drawString(pos, 30, 30);
+            g2.drawString(dest, 30, 60);
+            g2.drawString(route, 30, 90);
+
         }
     }
 
     /**
      * Added a new class that will execute the new route calculation
      * in a separate swing-friendly thread so the User Interface does not block
-     *
+     * <p/>
      * Fun.
      */
-    private class GetRouteCalculationWorker extends SwingWorker<String, Object> {
+    private class GetRouteCalculationWorker extends SwingWorker<String, Object>
+    {
         private final String routeString;
         private boolean wasSuccessful;
 
@@ -145,27 +189,28 @@ public class UI extends JPanel {
             try
             {
                 int destinationAsInteger = Integer.parseInt(routeString);
+//                confirmDestinationButton.setEnabled(false);
+                isProcessingNewRoute = true;
                 currentDestination = new Position(destinationAsInteger);
-                confirmDestinationButton.setEnabled(false);
                 rc.calculateRoute(currentDestination);
             }
             catch (Exception err)
             {
                 System.out.println("Error: " + err.getMessage());
-            }
-
-            finally
+            } finally
             {
+
                 return "Done doInBackground";
             }
         }
 
         @Override
-        protected void done() {
-//            try {
-////                label.setText(get());
-//            } catch (Exception ignore) {
-//            }
+        protected void done()
+        {
+            confirmDestinationButton.setEnabled(true);
+            isProcessingNewRoute = false;
+            repaint();
+
         }
     }
 }
